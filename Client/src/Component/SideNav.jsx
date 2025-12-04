b23f2f3ff75d445933b7d3e0f4a82eff6357aa0a
Dinesh <dineshsrinivasanr77529@gmail.com>
2025-12-05 00:21:02 +0530

    same

--- Client/src/Component/SideNav.jsx
+++ Client/src/Component/SideNav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../assets/Images/logo.png'
 
 export default function SideNav() {
+    const [sidebar, setSidebar] = useState(false);
     return (
     <> 
         <h1><img src={logo} alt="" className='w-15'/>Musify</h1>
